80a60ff0db464662de98ef83ca05fb2b4557246a
Cameron Prestera <cameronjohn@churchofjesuschrist.org>
2025-06-28 08:44:48 -0600

    Card view

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Cameron John Prestera on 6/22/25.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
