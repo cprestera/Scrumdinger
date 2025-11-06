bb0e46081deec05129d231bc2ec89025a7294f0b
Cameron Prestera <cameronjohn@churchofjesuschrist.org>
2025-11-06 09:07:43 -0700

    Error handling for scrum saving.

---
+++ Scrumdinger/Views/ErrorView.swift
@@ -0,0 +1,45 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by Cameron John Prestera on 11/6/25.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        VStack {
+            Text("An error has occurred!")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWrapper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+                .padding(.top)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("Dismiss") {
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+private enum SampleError: Error {
+    case errorRequired
+}
+
+#Preview {
+    ErrorView(errorWrapper: ErrorWrapper(error: SampleError.errorRequired, guidance: "You can safely ignore this error."))
+}
